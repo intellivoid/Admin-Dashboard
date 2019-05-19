178ff2aeb44c25811dd4c1b08ce8e324ae45fa4e
narrakas <zixing.narrakas1996@gmail.com>
2019-05-18 20:50:50 -0400

    added auth checker

--- src/resources/pages/login/scripts/check_existing_auth.php
+++ src/resources/pages/login/scripts/check_existing_auth.php
@@ -1,44 +1,7 @@
 <?php
 
-    use DynamicalWeb\DynamicalWeb;
-    use DynamicalWeb\HTML;
-    use sws\sws;
-
-    try
+    if(WEB_SESSION_ACTIVE == true)
     {
-        DynamicalWeb::loadLibrary('sws', 'sws', 'sws.php');
-    }
-    catch (Exception $e)
-    {
-        header('Location: /500');
+        header('Location: /');
         exit();
     }
-
-    $sws = new sws();
-
-    if($sws->WebManager()->isCookieValid('dashboard_session') == false)
-    {
-        $Cookie = $sws->CookieManager()->newCookie('dashboard_session', 86400, false);
-
-        $Cookie->Data = array(
-            'session_active' => false,
-        );
-
-        $sws->CookieManager()->updateCookie($Cookie);
-        $sws->WebManager()->setCookie($Cookie);
-
-        if($Cookie->Name == null)
-        {
-            print('There was an issue with the security check, Please refresh the page');
-            exit();
-        }
-
-        header('Refresh: 2; URL=/');
-        print('Loading WebApp Resources ...');
-        exit();
-
-    }
-
-    $Cookie = $sws->WebManager()->getCookie('dashboard_session');
-
-    define('WEB_SESSION_ACTIVE', $Cookie->Data['session_active'], false);
--- src/resources/pages/login/scripts/verify_auth.php
+++ src/resources/pages/login/scripts/verify_auth.php
@@ -1,10 +1,24 @@
 <?php
 
+use DynamicalWeb\DynamicalWeb;
+use DynamicalWeb\HTML;
+use sws\sws;
 
     if($_SERVER['REQUEST_METHOD'] == 'POST')
     {
         if(isset($_POST['authentication_code']))
         {
+
+            try
+            {
+                DynamicalWeb::loadLibrary('sws', 'sws', 'sws.php');
+            }
+            catch (Exception $e)
+            {
+                header('Location: /500');
+                exit();
+            }
+
             $Auth = \DynamicalWeb\DynamicalWeb::getConfiguration('authentication');
             if(hash('sha256', $_POST['authentication_code']) !== hash('sha256', $Auth['AUTHENTICATION_CODE']))
             {
@@ -12,7 +26,12 @@
                 exit();
             }
 
-            setcookie('authentication', $_POST['authentication_code'], 1800);
+            $sws = new sws();
+            $Cookie = $sws->WebManager()->getCookie('dashboard_session');
+            $Cookie->Data['session_active'] = true;
+            $sws->CookieManager()->updateCookie($Cookie);
+
+            define('WEB_SESSION_ACTIVE', $Cookie->Data['session_active'], false);
             header('Location: /');
             exit();
         }
