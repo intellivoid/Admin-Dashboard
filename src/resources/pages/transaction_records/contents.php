d0ab3a0d64f4c4064826d74a79e7a3abe3dda6f9
Narrakas <netkas@users.noreply.github.com>
2019-06-12 23:58:16 -0400

    Minor correction

--- src/resources/pages/transaction_records/contents.php
+++ src/resources/pages/transaction_records/contents.php
@@ -27,10 +27,7 @@
                     <div class="row">
                         <div class="x_panel">
                             <div class="x_title">
-                                <h2> OpenBlu VPN Servers
-                                    <small>Servers hosted by OpenBlu</small>
-                                </h2>
-
+                                <h2> Transaction Records </h2>
                                 <div class="clearfix"></div>
                             </div>
                             <div class="x_content">
