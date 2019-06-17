16d7b0666f3f59b71d2f4d5d799f0dfa52da9752
Narrakas <netkas@users.noreply.github.com>
2019-06-17 19:58:23 -0400

    Added Favicon

--- src/resources/sections/header.php
+++ src/resources/sections/header.php
@@ -4,6 +4,20 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
+<link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png?v=BG7PrBbaWk">
+<link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png?v=BG7PrBbaWk">
+<link rel="icon" type="image/png" sizes="194x194" href="/assets/favicon/favicon-194x194.png?v=BG7PrBbaWk">
+<link rel="icon" type="image/png" sizes="192x192" href="/assets/favicon/android-chrome-192x192.png?v=BG7PrBbaWk">
+<link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png?v=BG7PrBbaWk">
+<link rel="manifest" href="/assets/favicon/site.webmanifest?v=BG7PrBbaWk">
+<link rel="mask-icon" href="/assets/favicon/safari-pinned-tab.svg?v=BG7PrBbaWk" color="#323232">
+<link rel="shortcut icon" href="/assets/favicon/favicon.ico?v=BG7PrBbaWk">
+<meta name="apple-mobile-web-app-title" content="Intellivoid Admin">
+<meta name="application-name" content="Intellivoid Admin">
+<meta name="msapplication-TileColor" content="#00aba9">
+<meta name="msapplication-config" content="/assets/favicon/browserconfig.xml?v=BG7PrBbaWk">
+<meta name="theme-color" content="#ffffff">
+
 <!-- Bootstrap -->
 <link href="/assets/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 <!-- Font Awesome -->
