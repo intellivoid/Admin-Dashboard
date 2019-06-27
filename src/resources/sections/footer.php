f3df9dc23f9c1815bd166a0c5c35331935038860
Narrakas <netkas@users.noreply.github.com>
2019-06-27 17:39:51 -0400

    Updated footer

--- src/resources/sections/footer.php
+++ src/resources/sections/footer.php
@@ -1,6 +1,6 @@
 <footer>
     <div class="pull-right">
-        Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+        Intellivoid - 2017-2019 | <i class="fa fa-warning text-warning"></i> This dashboard is unstable
     </div>
     <div class="clearfix"></div>
 </footer>
